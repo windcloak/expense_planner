6ce39a7538e5cce4789bbf4aa087662c5cc02b9a
May Guo <40670609+windcloak@users.noreply.github.com>
2020-06-18 16:55:26 -0500

    Added transaction class

---
+++ lib/transaction.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart'; // for required
+
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+// constructor
+  Transaction({
+    @required this.id,
+    @required this.title,
+    @required this.amount,
+    @required this.date,
+  });
+}
