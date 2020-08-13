43a2d7878f31031633748971a3c13f63afb60f7c
windcloak <xfirecloak@gmail.com>
2020-08-13 18:07:32 -0500

    Split transaction item as its own widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
-import 'package:intl/intl.dart';
+import './transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -40,46 +40,8 @@ class TransactionList extends StatelessWidget {
                 itemBuilder: (ctx, index) {
                   // called for every new list item
 
-                  return Card(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 5,
-                    ),
-                    elevation: 5,
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: EdgeInsets.all(6),
-                          child: FittedBox(
-                            child: Text(
-                                '\$${transactions[index].amount.toStringAsFixed(2)}'),
-                          ),
-                        ),
-                      ),
-                      title: Text(
-                        transactions[index].title,
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      subtitle: Text(
-                        DateFormat.yMMMd().format(transactions[index].date),
-                      ),
-                      trailing: MediaQuery.of(context).size.width > 460
-                          ? FlatButton.icon(
-                              icon: Icon(Icons.delete),
-                              label: Text('Delete'),
-                              textColor: Theme.of(context).errorColor,
-                              onPressed: () => deleteTx(transactions[index].id),
-                            )
-                          : IconButton(
-                              icon: Icon(Icons.delete),
-                              color: Theme.of(context).errorColor,
-                              // onPressed normally doesn't pass arguments
-                              // Must wrap this in anoymous fx to pass
-                              onPressed: () => deleteTx(transactions[index].id),
-                            ),
-                    ),
-                  );
+                  return TransactionItem(
+                      transaction: transactions[index], deleteTx: deleteTx);
                 },
                 itemCount: transactions.length,
                 // children: transactions.map((transactions[index]) {
